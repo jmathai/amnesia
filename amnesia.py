5b01a328ea577ce65c6ff8f351703f3f1272135a
Trishank Karthik <trishankkarthik@users.noreply.github.com>
2019-09-28 12:22:44 -0400

    Work with the new Twitter archive format

--- amnesia.py
+++ amnesia.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import csv
+import json
 import sys
 import time
 from datetime import datetime, timedelta
 
 
 from configs import twitter as twitter_config
-from tweepy import API, Cursor, OAuthHandler
+from tweepy import API, OAuthHandler
 from tweepy.error import TweepError
 
 
@@ -48,26 +48,26 @@ if __name__ == "__main__":
     Compare the tweet's date with the threshold.
     If the tweet is older than the threshold we delete it.
    """
-    with open('tweets.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        # skip initial row
-        line = 0
-        for row in reader:
-            if line == 0:
-                pass
-            else:
-                status_id = row[0]
-                status_created_at = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S +0000")
-                status_text = row[5]
-                if status_created_at < delete_before:
-                    try:
-                        if not dry_run:
-                            api.destroy_status(status_id)
-                        print("Deleted {}".format(status_id))
-                        print(status_text.encode('utf-8'))
-                    except TweepError as err:
-                        print("Could not delete {}".format(status_id))
-            line += 1
+    with open('tweet.js') as tweet_js:
+        TWEET_PREFIX = 'window.YTD.tweet.part0 = '
+        tweet_js = tweet_js.read()
+        assert tweet_js.startswith(TWEET_PREFIX)
+        tweet_json = tweet_js[len(TWEET_PREFIX):]
+        tweets = json.loads(tweet_json)
+
+        for tweet in tweets:
+            status_id = tweet['id']
+            status_created_at = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+            status_text = tweet['full_text']
+
+            if status_created_at < delete_before:
+                try:
+                    if not dry_run:
+                        api.destroy_status(status_id)
+                    print("Deleted {}".format(status_id))
+                    print(status_text.encode('unicode_escape'))
+                except TweepError as err:
+                    print("Could not delete {}".format(status_id))
 
     """
     Loop over your favorites.
@@ -79,15 +79,25 @@ if __name__ == "__main__":
     RATE_LIMIT = 180
     counter, elapsed_time, previous_time = 0, 0, 0
 
-    for status in Cursor(api.favorites).items():
-        if status.created_at < delete_before:
+    with open('like.js') as like_js:
+        LIKE_PREFIX = 'window.YTD.like.part0 = '
+        like_js = like_js.read()
+        assert like_js.startswith(LIKE_PREFIX)
+        like_json = like_js[len(LIKE_PREFIX):]
+        likes = json.loads(like_json)
+
+        for like in likes:
+            like = like['like']
+            status_id = like['tweetId']
+            status_text = like['fullText']
+
             try:
                 if not dry_run:
-                    api.destroy_favorite(status.id)
-                print("Un-favorited {}".format(status.id))
-                print(status.text.encode('utf-8'))
+                    api.destroy_favorite(status_id)
+                print("Unliked {}".format(status_id))
+                print(status_text.encode('unicode_escape'))
             except TweepError as err:
-                print("Could not un-favorite {}".format(status.id))
+                print("Could not unlike {}".format(status_id))
             finally:
                 # Sleep for a bit if we exceed the rate limit.
                 current_time = time.time()
@@ -95,8 +105,7 @@ if __name__ == "__main__":
                 elapsed_time = current_time-previous_time
                 previous_time = current_time
                 counter += 1
+
                 if elapsed_time < FIFTEEN_MINUTES and counter > RATE_LIMIT:
                     time.sleep(FIFTEEN_MINUTES-elapsed_time)
                     counter, elapsed_time, previous_time = 0, 0, 0
-
-
